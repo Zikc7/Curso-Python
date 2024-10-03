ff6a742b084c4a1a12fb3aa58cc3ac0128a4eb9e
Santiago <santimir12@gmail.com>
2024-10-03 00:25:11 -0300

    Agregar .gitignore y "modulacion"

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello Python")
+
+from subir_ejercicio_colabs import promedio_suma
+
+#! Ejercicio solicitado en la clase asincronica 3 modulo 1: 
+promedio_suma()
+
+
---
+++ subir_ejercicio_colabs.py
@@ -0,0 +1,18 @@
+
+def promedio_suma():
+  suma = 0
+  i = 0
+  print("Ingrese los numeros que desee promediar y sumar: ")
+  while True:
+    number : int = int(input(" "))
+
+    suma: int = suma + number
+    if number != 0:
+      i = i + 1
+
+    if number == 0:
+      promedio = suma / i
+      print(f"La suma de los numeros es {suma}")
+
+      print(f"El promedio de los numeros es {promedio}")
+      break
